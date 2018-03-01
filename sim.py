3ef10f92124df3a3aa1293791a0ccc2f7cabc303
Matthew De Angelis <m@ttda.io>
2018-03-01 18:19:37 +0000

    Start of main simulation file

---
+++ sim.py
@@ -0,0 +1,13 @@
+def updateFCars(list):
+    fcList = []
+    for each x in list:
+        if(!x.is_moving):
+            fcList.append(x)
+    return fcList;
+
+
+
+
+list of rides
+
+assign closest ride
