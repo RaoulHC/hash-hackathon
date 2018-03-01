5f20b1236e9e6afc0103aa1d76f25e969ac60e2a
Raoul Hidalgo Charman <raoul.hidalgo.charman@cern.ch>
2018-03-01 20:20:22 +0000

    test_car.py

--- test_car.py
+++ test_car.py
@@ -1,5 +1,6 @@
 from car import Car
 from queue import RideQueue
+from ride import Ride
 
 rq = RideQueue([])
 c = Car()
@@ -7,3 +8,30 @@ c.step
 
 print c.pos
 print c.ride
+
+# lets try some basic ride queue with one thing
+
+r1 = Ride((1, 1), (2, 2), 0, 10, 1)
+rq = RideQueue(r1)
+
+print r1
+c.initialise_ride_queue([])
+print c.get_ride_queue()
+
+# hack a ride onto it for now
+c.ride = r1
+
+print "Car stuff"
+print "position: ", c.pos
+print "ride: ", c.ride
+print "riding: ", c.riding
+print "time remaining: ", c.time_remaining
+print
+
+c.ride_step()
+
+print "Car stuff"
+print "position: ", c.pos
+print "ride: ", c.riding
+print "time remaining: ", c.time_remaining
+
