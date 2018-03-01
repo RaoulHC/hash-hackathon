828418f695a7a3bcfdc4496e8ebc6d2868d208b8
JacobRawling <mrjacob3@gmail.com>
2018-03-01 19:41:31 +0000

    Renaming variables in queue

--- queue.py
+++ queue.py
@@ -2,19 +2,30 @@ from copy import deepcopy
 # queue class file
 
 class RideQueue(object):
+    """
 
+    """
     def __init__(self, ride_list):
+        """
+        """
         self.ride_queue = ride_list
         self.ride_map = {}
 
     def give_ride(x, y, car_id):
-        # Iterate over
+        """
+        Give a car located at x,y the closest ride to it
+        """
+
+        # Start with the first ride in the queue as a trial solution.
+        best_distance = abs(x - ride_queue[0].start_pos[0]) + abs(y - ride_queue[0].start_pos[1])
         current_ride = 0
-        res = abs(x - ride_queue[0].start_pos[0]) + abs(y - ride_queue[0].start_pos[1])
+
+        #
         for i in xrange(len(ride_queue)):
-            dist = abs(x - ride_queue[i].start_pos[0]) + abs(y - ride_queue[i].start_pos[1])
-            if dist < res:
-                res = dist
+            # for each ride evaluate the distance between the car and the ride
+            distance_to_car = abs(x - ride_queue[i].start_pos[0]) + abs(y - ride_queue[i].start_pos[1])
+            if distance_to_car < best_distance:
+                best_distance = distance_to_car
                 current_ride = i
 
         # Store the assigned ride's id to the final list
