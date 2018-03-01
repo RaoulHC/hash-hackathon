305b8dd3bc0888a54d36d9db9924ec190189a7dd
JacobRawling <mrjacob3@gmail.com>
2018-03-01 18:49:50 +0000

    Added id to ride. Added car creation to simulation manager.

--- evaluate_answer.py
+++ evaluate_answer.py
@@ -1,3 +1,13 @@
-from input_manager import InputManager
+from simulation_manager import SimulationManager
 
-input_man = InputManager('in/a_example.in')
+input_man = SimulationManager('in/a_example.in')
+
+for ride in input_man.rides:
+    print "RIDE FROM ", ride.start_pos, " TO ", ride.end_pos
+
+
+# Run the simulation
+for t in xrange(input_man.T):
+    for car in input_man.cars:
+        car.step()
+
