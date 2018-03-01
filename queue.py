3cd9be6562513c1f50b94b688c1b08543c0805b5
JacobRawling <mrjacob3@gmail.com>
2018-03-01 20:08:25 +0000

    Not sure.

--- queue.py
+++ queue.py
@@ -11,8 +11,12 @@ class RideQueue(object):
         self.ride_queue = ride_list
         self.ride_map = {}
 
-    def distance(self,ride, x,y):
-        return abs(x - ride.start_pos[0]) + abs(y - ride.start_pos[1])
+    def distance(self, ride, x,y):
+        manhat_dist = abs(x - ride.start_pos[0]) + abs(y - ride.start_pos[1])
+        # the time a car will wait can be added to the distance between a car
+        # and a ride as it can be thought of as an effective additional distance
+        wait_distance = min(0,ride.earliest_step - Car.t - manhat_dist)
+        return manhat_dist + wait_distance
 
     def give_ride(sekf, x, y, car_id):
         """
@@ -24,7 +28,7 @@ class RideQueue(object):
         """
 
         # Start with the first ride in the queue as a trial solution.
-        best_distance = distance(self.ride_queue[0])
+        best_distance = self.distance(self.ride_queue[0])
         current_ride = 0
 
         #
